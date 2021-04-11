7f086bd6e9ff0a876d6e3bb4926477a291a7b1db
enPlace <n.place@protonmail.com>
2021-04-11 13:56:50 -0300

    tab display and styleing

--- notes.md
+++ notes.md
@@ -1,4 +1,5 @@
-ul, each li will be the tab 
+ul, each li will be the tab. 
+in the li attributes, we'll put a data tag that targets the id of each div below with the tab content
 
 div class of tabs content
 in this div, make other divs that hold the content of each individual tab 
--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContent = document.querySelectorAll('[data-tab-content]')
 
 tabs.forEach(tab =>{
     tab.addEventListener('click', e=>{
         const target = document.querySelector(tab.dataset.tabTarget)
+        tabContent.forEach(content=>{
+            content.classList.remove('active')
+        })
         target.classList.add('active')
         console.log(target.className)
 
