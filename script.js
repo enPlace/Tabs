9d3cfc2df03c28c1605118a71458c53465388918
enPlace <n.place@protonmail.com>
2021-04-11 15:02:44 -0300

    adding a new tab function

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 const tabs = document.querySelectorAll('[data-tab-target]')
 const tabContent = document.querySelectorAll('[data-tab-content]')
+const tabtargs = document.querySelectorAll(tabs[0].dataset.tabTarget)
+tabList = document.querySelector(".tablist")
 
 tabs.forEach(tab =>{
     tab.addEventListener('click', e=>{
         const target = document.querySelector(tab.dataset.tabTarget)
+        
         tabContent.forEach(content=>{
             content.classList.remove('active')
         })
         target.classList.add('active')
         console.log(target.className)
+        tabs.forEach(tab =>{
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
 
     })
+})
+newTab = document.querySelector(".newtab")
+newTab.addEventListener('submit', e=>{
+    e.preventDefault()
+    // make a new tab at the top//
+    const listLength = document.querySelectorAll("li").length
+    const inputValue = document.querySelector("#tabname").value
+    const newLi = document.createElement("li")
+    newLi.textContent= inputValue
+    newLi.classList.add("tab")
+    newLi.setAttribute("data-tab-target", `#newtab${listLength}`)
+    tabList.appendChild(newLi)
+    console.log(listLength)
 })
